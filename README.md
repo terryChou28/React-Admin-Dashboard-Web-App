b856d40560aeea18b11f85046d8a9891932d9edd
Terry <chouterr@students.cs.ubc.ca>
2022-05-15 02:14:42 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # React-Admin Dashboard Web App
 
+![add](dashboard.gif)
+
 ## Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/widgetLg/WidgetLg.jsx
+++ src/components/widgetLg/WidgetLg.jsx
@@ -18,7 +18,7 @@ export default function WidgetLg() {
         <tr className="widgetLgTr">
           <td className="widgetLgUser">
             <img
-            src="" 
+            src="https://scontent.fyvr3-1.fna.fbcdn.net/v/t1.6435-9/137051684_4241554922525405_2757720841713573760_n.jpg?_nc_cat=107&ccb=1-6&_nc_sid=09cbfe&_nc_ohc=RjpT5aAIKGAAX9cnBhJ&_nc_ht=scontent.fyvr3-1.fna&oh=00_AT8OVq3uTugL6WAv59iZWlIizMLyGhJ8253vbMHmAwPwhg&oe=62A61453" 
             alt="" 
             className="widgetLgImg" 
             />
@@ -33,7 +33,7 @@ export default function WidgetLg() {
         <tr className="widgetLgTr">
           <td className="widgetLgUser">
             <img
-            src="" 
+            src="https://scontent.fyvr3-1.fna.fbcdn.net/v/t1.18169-9/14993337_10154623153658954_2249240204065324204_n.jpg?_nc_cat=108&ccb=1-6&_nc_sid=09cbfe&_nc_ohc=QIahInzC3JcAX95peek&_nc_ht=scontent.fyvr3-1.fna&oh=00_AT_mCcYEPWDJ5wHF3Ru4m6g2FN2swQPHWYIIy8rDSVUYRw&oe=62A491FA" 
             alt="" 
             className="widgetLgImg" 
             />
@@ -48,7 +48,7 @@ export default function WidgetLg() {
         <tr className="widgetLgTr">
           <td className="widgetLgUser">
             <img
-            src="" 
+            src="https://scontent.fyvr3-1.fna.fbcdn.net/v/t1.18169-9/14993337_10154623153658954_2249240204065324204_n.jpg?_nc_cat=108&ccb=1-6&_nc_sid=09cbfe&_nc_ohc=QIahInzC3JcAX95peek&_nc_ht=scontent.fyvr3-1.fna&oh=00_AT_mCcYEPWDJ5wHF3Ru4m6g2FN2swQPHWYIIy8rDSVUYRw&oe=62A491FA" 
             alt="" 
             className="widgetLgImg" 
             />
@@ -63,7 +63,7 @@ export default function WidgetLg() {
         <tr className="widgetLgTr">
           <td className="widgetLgUser">
             <img
-            src="" 
+            src="https://scontent.fyvr3-1.fna.fbcdn.net/v/t1.6435-9/137051684_4241554922525405_2757720841713573760_n.jpg?_nc_cat=107&ccb=1-6&_nc_sid=09cbfe&_nc_ohc=RjpT5aAIKGAAX9cnBhJ&_nc_ht=scontent.fyvr3-1.fna&oh=00_AT8OVq3uTugL6WAv59iZWlIizMLyGhJ8253vbMHmAwPwhg&oe=62A61453" 
             alt="" 
             className="widgetLgImg" 
             />
